ad3d249bd6805186497313d73eb462fe71b2fdb9
RitikRohin <irthaiwan@gmail.com>
2025-07-27 21:11:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-BOT_USERNAME = getenv("BOT_USERNAME", "EsproChatBot")
+BOT_USERNAME = getenv("BOT_USERNAME", "8010493541")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7666870729").split()))
 
